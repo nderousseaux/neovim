dba95e7001fbd5ce2886c656d6ed41fd622dbb6e
Derousseaux Nathanaël <n.derousseaux@icloud.com>
2023-04-23 14:54:16 +0200

    Ajout astro

--- init.lua
+++ init.lua
@@ -1,13 +1,20 @@
 if vim.loader then vim.loader.enable() end -- enable vim.loader early if available
 
--- On charge les sources
 for _, source in ipairs {
-  "settings",
-  "plugins-manager",
-  "colorschemes",
+  "astronvim.bootstrap",
+  "astronvim.options",
+  "astronvim.lazy",
+  "astronvim.autocmds",
+  "astronvim.mappings",
 } do
   local status_ok, fault = pcall(require, source)
   if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
 end
--- Même couleur que les commentaires
-vim.api.nvim_set_hl(0, "CopilotSuggestion", { fg = "#5c6370"})
+
+if astronvim.default_colorscheme then
+  if not pcall(vim.cmd.colorscheme, astronvim.default_colorscheme) then
+    require("astronvim.utils").notify("Error setting up colorscheme: " .. astronvim.default_colorscheme, "error")
+  end
+end
+
+require("astronvim.utils").conditional_func(astronvim.user_opts("polish", nil, false), true)
